8edeb757c200746cbcbab19919cc47d9b8c0dfe8
Tiago Pestana <tnevespestana@gmail.com>
2025-09-14 01:45:58 +0100

    update to captains log formula

--- captainslog.rb
+++ captainslog.rb
@@ -1,5 +1,15 @@
-def install
-  bin.install "bin/log"
-  libexec.install "src/main.lua"
-  inreplace bin/"log", "__MAIN_LUA__", libexec/"main.lua"
+class Captainslog < Formula
+  desc "Markdown logging tool"
+  homepage "https://github.com/yourusername/CaptainsLog"
+  url "https://github.com/yourusername/CaptainsLog/archive/v0.1.0.tar.gz"
+  sha256 "<FILL_THIS_IN>"
+  license "MIT"
+
+  depends_on "lua"
+
+  def install
+    bin.install "bin/log"
+    libexec.install "src/main.lua"
+    inreplace bin/"log", "__MAIN_LUA__", libexec/"main.lua"
+  end
 end
