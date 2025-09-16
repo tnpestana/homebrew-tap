b798a93cb16ec22bd359518bd4b92b7edfea2cac
Tiago Pestana <tnevespestana@gmail.com>
2025-09-16 18:56:28 +0100

    Update captains-log version

--- Formula/captains-log.rb
+++ Formula/captains-log.rb
@@ -1,9 +1,9 @@
 class CaptainsLog < Formula
   desc "Markdown log tool"
   homepage "https://github.com/tnpestana/captains-log"
-  url "https://github.com/tnpestana/captains-log/archive/v1.0.1.tar.gz"
+  url "https://github.com/tnpestana/captains-log/archive/v1.0.2.tar.gz"
   sha256 "54b7b3893ae2366239205224e72c285b3bf2d0fd040da450a5ba21c81937823d"  
-  version "1.0.1"
+  version "1.0.2"
 
   depends_on "lua"
 
