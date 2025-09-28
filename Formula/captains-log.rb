3731e14ea1d9236fba5cc512a6bc5efe9c865dc5
Tiago Pestana <tnevespestana@gmail.com>
2025-09-28 23:09:08 +0100

    update clog version

--- Formula/captains-log.rb
+++ Formula/captains-log.rb
@@ -1,9 +1,10 @@
 class CaptainsLog < Formula
   desc "Markdown log tool"
   homepage "https://github.com/tnpestana/captains-log"
-  url "https://github.com/tnpestana/captains-log/archive/v1.0.2.tar.gz"
-  sha256 "f24ed0f219ceab1785f9c92896c952bc1fab08a89f81607ca9d59b4bb20fe373"  
-  version "1.0.2"
+  url "https://github.com/tnpestana/captains-log/archive/v1.0.3.tar.gz"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  
+  version "1.0.3"
 
   depends_on "lua"
 
